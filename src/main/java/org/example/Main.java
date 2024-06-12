60ecd2e8897087e01092f3e6b0dbfb283eace5a2
DhruvSingh861 <singhdhruv861@gmail.com>
2024-06-12 10:58:52 +0530

    changed the code to set and then get value from redis.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,9 @@ import redis.clients.jedis.Jedis;
 
 public class Main {
     public static void main(String[] args) {
-        Jedis jedis=new Jedis();
-        System.out.println(jedis.get("name"));
+        Jedis jedis=new Jedis("localhost",6379);
+        jedis.set("name","Dhruv");
+        jedis.set("role","ASE");
+        System.out.println(jedis.get("name")+" : "+jedis.get("role"));
     }
 }
