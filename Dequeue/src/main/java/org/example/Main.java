d1de468e46cc379e7b46aebd7e0025e5b045262b
DhruvSingh861 <singhdhruv861@gmail.com>
2024-06-13 15:44:56 +0530

    Reliable Queue also implemented

--- Dequeue/src/main/java/org/example/Main.java
+++ Dequeue/src/main/java/org/example/Main.java
@@ -2,12 +2,29 @@ package org.example;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Jedis jedis=new Jedis("localhost",6379);
+
+        //removing from simple queue.
         for(int i=0;i<=10;i++){
             System.out.println(jedis.rpop("queue"));
         }
-        System.out.println("Queue is Empty now");
+
+        //Reliable queue
+        // a task is not removed from the queue immediately when it is dequeued.
+        // Instead, it is moved to a temporary queue where it is stored until
+        // the consumer confirms that the task has been processed
+        for(int i=0;i<=10;i++){
+            //RPOPLPUSH source destination
+            jedis.rpoplpush("reliableQueue","tempQueue");
+        }
+
+        System.out.println("queue and reliableQueue is Empty now");
+        System.out.println("Data is still present in the tempQueue. ->");
+        List<String> l=jedis.lrange("tempQueue",0,10);
+        System.out.println(l);
     }
 }
--- Enque/src/main/java/org/example/Main.java
+++ Enque/src/main/java/org/example/Main.java
@@ -4,10 +4,13 @@ import redis.clients.jedis.Jedis;
 
 public class Main {
     public static void main(String[] args) {
+        //simple redis list as a queue
         Jedis jedis=new Jedis("localhost",6379);
         for(int i=0;i<=10;i++){
             jedis.lpush("queue","Data:"+i);
+            jedis.lpush("reliableQueue","Data:"+i);
         }
-        System.out.println("10 entries is Enqued.");
+
+        System.out.println("10 entries is Enqued in 'queue' and 'reliableQueue'.");
     }
 }
